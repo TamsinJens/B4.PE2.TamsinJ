0f19b3d463a6b04c85e605707e9d1ceb7612292c
TamsinJens <jens.tamsin@student.howest.be>
2019-11-01 22:16:30 +0100

    Make interface

---
+++ B4.PE2.TamsinJ/B4.PE2.TamsinJ/Domain/Services/IConversionService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace B4.PE2.TamsinJ.Domain.Services
+{
+    interface IConversionService
+    {
+        int HoursToDays(int hours);
+        int DaysToHours(int days);
+        int MinutesToHours(int minutes);
+        int HoursToMinutes(int hours);
+        int SecondsToMinutes(int seconds);
+        int MinutesToSeconds(int minutes);
+    }
+}
