7bd83977d19cde6bef887d75ca2578b72a29ced3
TamsinJens <jens.tamsin@student.howest.be>
2019-11-01 23:49:58 +0100

    Make submitcontrol of feedbackform

--- B4.PE2.TamsinJ/B4.PE2.TamsinJ/Feedback.xaml.cs
+++ B4.PE2.TamsinJ/B4.PE2.TamsinJ/Feedback.xaml.cs
@@ -16,5 +16,45 @@ namespace B4.PE2.TamsinJ
         {
             InitializeComponent();
         }
+
+        public async void btnSubmit_ClickedAsync(object sender, EventArgs e)
+        {
+            if (SubmitControl())
+            {
+                //email form
+                await DisplayAlert("Succes", "Your form was succesfully sent to us.", "ok.");
+            }
+            else
+            {
+                await DisplayAlert("Oops", "You didn't fill in the form properly. Check the form and change where necessary." , "Return");
+            }
+        }
+
+        public bool SubmitControl()
+        {
+            if(inputNaam.Text.Length > 0)
+            {
+                if(inputVoornaam.Text.Length > 0)
+                {
+                    if(inputemail.Text.Length > 0) //betere controle nodig
+                    {
+                        if(inputtel.Text.Length == 10)
+                        {
+                            if(inputdate != null)
+                            {
+                                if(picker != null)
+                                {
+                                    if(inputbericht.Text.Length > 0)
+                                    {
+                                        return true;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            return false;
+        }
     }
 }
