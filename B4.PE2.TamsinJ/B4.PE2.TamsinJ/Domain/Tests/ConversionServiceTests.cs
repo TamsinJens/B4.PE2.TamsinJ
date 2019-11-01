362c45e58640832ed62bb348496db24697077118
TamsinJens <jens.tamsin@student.howest.be>
2019-11-01 22:45:45 +0100

    Try Unittest

---
+++ B4.PE2.TamsinJ/B4.PE2.TamsinJ/Domain/Tests/ConversionServiceTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace B4.PE2.TamsinJ.Domain.Tests
+{
+    class ConversionServiceTests
+    {
+        int[] Integerinputs;
+        ConversionService cs = new ConversionService();
+
+        public ConversionServiceTests()
+        {
+            Integerinputs = TestData.Integerinputs;
+        }
+
+        [Fact]
+        public void DaysToHours_Returns_IntOfHours()
+        {
+            var integer = Integerinputs[0];
+            int hours = cs.DaysToHours(integer);
+            Assert.Equal(hours, 120);
+        }
+    }
+}
---
+++ B4.PE2.TamsinJ/B4.PE2.TamsinJ/Domain/Tests/TestData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace B4.PE2.TamsinJ
+{
+    class TestData
+    {
+        public static int[] Integerinputs => new int[]
+        {
+            5,6,7,10,11
+        };
+    }
+}
